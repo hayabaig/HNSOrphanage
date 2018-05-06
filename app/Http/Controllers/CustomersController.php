582661ca098a43393c5d88846495c4cca2fac01d
hayabaig <hayabaigmirza95@gmail.com>
2018-05-06 19:44:27 +0500

    second commit

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Customers;
 
 class CustomersController extends Controller
 {
@@ -13,7 +14,8 @@ class CustomersController extends Controller
      */
     public function index()
     {
-        //
+        return Customers::all();
+        return view('customers.index');
     }
 
     /**
